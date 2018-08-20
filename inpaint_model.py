20c5315e9896ddbcd09912289519eb975464f21b
ItaiShchorry <itaishch@gmail.com>
2018-08-20 21:40:09 +0300

    without contextual attention

--- inpaint_model.py
+++ inpaint_model.py
@@ -85,14 +85,8 @@ class InpaintCAModel(Model):
             x = gen_conv(x, 4*cnum, 3, rate=4, name='xconv8_atrous')
             x = gen_conv(x, 4*cnum, 3, rate=8, name='xconv9_atrous')
             x = gen_conv(x, 4*cnum, 3, rate=16, name='xconv10_atrous')
+            x_hallu = x 
             
-			#turn off hallucination pathway for ablation study
-            zeros = tf.zeros(
-    shape=x.shape,
-    dtype=tf.float32,
-    name=None
-)
-            x_hallu = tf.multiply(x,zeros) #bit-wise multiplication			
             # attention branch
             x = gen_conv(xnow, cnum, 5, 1, name='pmconv1')
             x = gen_conv(x, cnum, 3, 2, name='pmconv2_downsample')
@@ -104,7 +98,15 @@ class InpaintCAModel(Model):
             x, offset_flow = contextual_attention(x, x, mask_s, 3, 1, rate=2)
             x = gen_conv(x, 4*cnum, 3, 1, name='pmconv9')
             x = gen_conv(x, 4*cnum, 3, 1, name='pmconv10')
-            pm = x
+            
+			#turn off attention pathway for ablation study
+            zeros = tf.zeros(
+    shape=x.shape,
+    dtype=tf.float32,
+    name=None
+)
+    
+            pm = tf.multiply(x,zeros)
             x = tf.concat([x_hallu, pm], axis=3)
 
             x = gen_conv(x, 4*cnum, 3, 1, name='allconv11')
